f4f53fc323a18040f30fa8f0e2109bf93840170e
Andrei Livadariu <mal@modernweb.ro>
2018-01-07 04:50:05 +0200

    Rewrote the example sketches using Reactduino

--- light-sensor/light-sensor.ino
+++ light-sensor/light-sensor.ino
@@ -1,23 +1,24 @@
+#include <Reactduino.h>
+
 #include <train.h>
 #include <lightsensor.h>
 
 Train train(8, TrainColor::Red, TrainChannel::One);
 LightSensor sensor(A0, 100);
 
-void setup() {
+Reactduino app([] {
   train.forward(TrainSpeed::Two);
-}
 
-void loop() {
-  sensor.loop();
+  app.onTick([] {
+    sensor.loop();
 
-  if (sensor.isJustCovered()) {
-    train.stop();
+    if (sensor.isJustCovered()) {
+      train.stop();
 
-    delay(2000);
-    train.reverse();
-    train.start();
-  }
-
-  delay(10);
-}
+      app.delay(2000, [] {
+        train.reverse();
+        train.start();
+      });
+    }
+  });
+});
--- start-stop/start-stop.ino
+++ start-stop/start-stop.ino
@@ -1,20 +1,22 @@
+#include <Reactduino.h>
+
 #include <train.h>
 
 Train train(8, TrainColor::Red, TrainChannel::One);
 
-void setup() {}
-
-void loop() {
+Reactduino app([] {
   train.forward(TrainSpeed::One);
-  delay(10000);
-  
-  train.stop();
-  delay(1000);
-  
-  train.reverse();
-  train.start();
-  delay(10000);
-
-  train.stop();
-  delay(1000);
-}
+  app.repeat(22000, [] { 
+    train.start();
+    app.delay(10000, [] {
+      train.stop();
+    });
+    app.delay(11000, [] {
+      train.reverse();
+      train.start();
+    });
+    app.delay(21000, [] {
+      train.stop();
+    });
+  });
+});

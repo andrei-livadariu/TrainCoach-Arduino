a48e859d2c2ad0fcbb9242efaf3b09f1233de512
Andrei Livadariu <mal@modernweb.ro>
2018-02-26 02:40:22 +0200

    Added a class that can drive a LEGO 9V power functions motor directly, provided that there is some way of safely hooking it up to the Arduino (probably some voltage-regulated circuit)

---
+++ TrainLib/src/lego9vmotor.cpp
@@ -0,0 +1,57 @@
+#include "lego9vmotor.h"
+
+Lego9VMotor::Lego9VMotor(byte c1, byte c2)
+    : _c1(c1), _c2(c2)
+{
+    pinMode(_c1, OUTPUT);
+    pinMode(_c2, OUTPUT);
+}
+
+byte Lego9VMotor::getSpeed()
+{
+    return _speed;
+}
+
+void Lego9VMotor::setSpeed(byte speed)
+{
+    _speed = speed;
+}
+
+MotorDirection Lego9VMotor::getDirection()
+{
+    return _direction;
+}
+
+void Lego9VMotor::setDirection(MotorDirection direction)
+{
+    _direction = direction;
+    if (isRunning()) {
+        start();
+    }
+}
+
+bool Lego9VMotor::isRunning()
+{
+    return _isRunning;
+}
+
+void Lego9VMotor::start()
+{
+    if (_direction == MotorDirection::Forward) {
+        // We are sending the commands in this order because we want to avoid having PWM / PWM at any time
+        analogWrite(_c2, 0);
+        analogWrite(_c1, _speed);
+    } else {
+        analogWrite(_c1, 0);
+        analogWrite(_c2, _speed);
+    }
+
+    _isRunning = true;
+}
+
+void Lego9VMotor::stop()
+{
+    analogWrite(_c1, 0);
+    analogWrite(_c2, 0);
+    _isRunning = false;
+}
---
+++ TrainLib/src/lego9vmotor.h
@@ -0,0 +1,32 @@
+#ifndef TRAINLIB_LEGO9VMOTOR_H
+#define TRAINLIB_LEGO9VMOTOR_H
+
+#include <Arduino.h>
+#include "imotor.h"
+
+class Lego9VMotor : public IMotor
+{
+    public:
+        Lego9VMotor(byte c1, byte c2);
+
+        byte getSpeed() override;
+        void setSpeed(byte speed) override;
+
+        MotorDirection getDirection() override;
+        void setDirection(MotorDirection direction) override;
+
+        bool isRunning() override;
+
+        void start() override;
+        void stop() override;
+
+    private:
+        const byte _c1;
+        const byte _c2;
+
+        MotorDirection _direction = MotorDirection::Forward;
+        byte _speed = 255;
+        bool _isRunning = false;
+};
+
+#endif

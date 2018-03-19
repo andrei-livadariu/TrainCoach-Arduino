cdeca6e5f51d9bef843f7c2d9ed99e1faff3e961
Andrei Livadariu <mal@modernweb.ro>
2018-03-19 21:00:22 +0200

    The track switch example is now based on a sensor

--- trackswitch/trackswitch.ino
+++ trackswitch/trackswitch.ino
@@ -1,16 +1,27 @@
 #include <Reactduino.h>
 #include <motorcontroller.h>
+#include <lightsensor.h>
 #include <trackswitch.h>
 
+LightSensor lightSensor(A0, 200);
 MotorController motor(7, 8, 9);
 TrackSwitch trackSwitch(motor);
+byte ledPin = 2;
 
 Reactduino app([] {
   app.onTick([] {
+    lightSensor.loop();
     trackSwitch.loop();
-  });
 
-  app.repeat(3000, [] { 
-    trackSwitch.flip();
+    if (lightSensor.isJustCovered() && !trackSwitch.isSwitching()) {
+      trackSwitch.flip();
+      lightSensor.sleep(4000);
+    }
+
+    if (lightSensor.isCovered()) {
+      digitalWrite(ledPin, HIGH);
+    } else {
+      digitalWrite(ledPin, LOW);
+    }
   });
 });
